0bcb274380e894d83f1727fe1a27dda91f7127a8
Yasir326 <yasirk@nandos.co.uk>
2020-04-21 15:52:26 +0100

    updated description for env var

--- src/_data/news.js
+++ src/_data/news.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 module.exports = async function() {
   try {
-    const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=gb&apiKey=${process.env.API_KEY}&PageSize=5`);
+    const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=gb&apiKey=${process.env.NEWS_API_KEY}&PageSize=5`);
     return response.data;
   } catch (error) {
     console.error(error);
